942ffd82c609c80183d110f77fc74e237f64522b
Tomohisa Kusano <siomiz@gmail.com>
2017-08-16 14:54:36 -0700

    wait until the debug port opens

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 google-chrome --headless --hide-scrollbars --remote-debugging-port=9222 --no-sandbox --disable-gpu &
 
-# need to sleep to allow chrome time to start
-sleep 1s
+# wait until the debug port opens
+while : ; do
+  set +e
+  (echo > /dev/tcp/127.0.0.1/9222) >/dev/null 2>&1
+  [[ $? -eq 0 ]] && break
+  set -e
+  sleep 1
+done
 
 node index.js --outputDir=/var/output/ "$@"
